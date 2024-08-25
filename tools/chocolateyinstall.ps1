87319b8585f7c6ce8fbb408605820743957b1749
rudesome <rudesome@users.noreply.github.com>
2024-08-25 18:33:21 +0000

    Updated to version v3.1.1
    Files changed:
    M	tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 $ErrorActionPreference = 'Stop';
-Install-ChocolateyPackage -packageName 'glazewm' -FileType exe -SilentArgs '/silent' -Url 'https://github.com/glzr-io/glazewm/releases/download/v3.1.1/glazewm-.exe' -checksum '' -checksumType 'sha256'
+Install-ChocolateyPackage -packageName 'glazewm' -FileType exe -SilentArgs '/silent' -Url 'https://github.com/glzr-io/glazewm/releases/download/v3.1.1/glazewm-3.1.1.exe' -checksum '' -checksumType 'sha256'
