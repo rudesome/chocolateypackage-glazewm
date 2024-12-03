448be28fac9da89beba6935b2e715e6df6a7c7c3
rudesome <rudesome@users.noreply.github.com>
2024-12-03 09:20:42 +0000

    update(version): v3.7.0
    Files changed:
    M	VERSION
    M	glazewm.nuspec
    M	tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $params = @{
   packageName    = 'glazewm'
   FileType       = 'msi'
   SilentArgs     = '/qn /norestart'
-  Url64bit       = 'https://github.com/glzr-io/glazewm/releases/download/v3.6.0/standalone-glazewm-v3.6.0-x64.msi'
-  checksum64     = '762a0ef381ddba009d14061775719a76c692cba3f697cc0a68403a6df849139c' 
+  Url64bit       = 'https://github.com/glzr-io/glazewm/releases/download/v3.7.0/standalone-glazewm-v3.7.0-x64.msi'
+  checksum64     = '733c09b3d19bf68fd5a522ee8f068f48cade8ec7422a4aa8a0d18b2beebe3494' 
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @params
